b2dbae282cec59a0b19205c5af712ab4b4959767
midhorner <horner.andrew.e@gmail.com>
2021-04-13 13:54:25 -0400

    presentational and container components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -33,14 +33,16 @@ class CampsiteInfo extends Component {
   }
   render() {
     if(this.props.campsite) {
-      return <div className="row">
-        {this.renderCampsite(this.props.campsite)}
-        {this.renderComments(this.props.campsite.comments)}
+      return (
+      <div className="container">
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
+        </div>
       </div>
+      );
     }
-    return(
-      <></>
-    );
+    return <></>;
   }
 }
 
