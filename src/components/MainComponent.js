44eefb17d56d1a29fa3fe8e761010cabd04b0115
midhorner <horner.andrew.e@gmail.com>
2021-04-13 22:46:57 -0400

    more crumbs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-// import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent'
 import Contact from './ContactComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS} from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
-import CampsiteInfo from './CampsiteInfoComponent';
+
 
 class Main extends Component {
   constructor(props) {
