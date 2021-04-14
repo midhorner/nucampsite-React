163126e306638ab30091fc396be398188b5a5840
midhorner <horner.andrew.e@gmail.com>
2021-04-13 20:15:15 -0400

    fix

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
-import CampsiteInfo from './CampsiteInfoComponent';
+// import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
   constructor(props) {
